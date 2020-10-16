53d7d472a7b2274fb0a78ab8d0d39240767c2377
aucls <aucls@outlook.com>
2020-10-16 13:35:17 -0300

    Escrevendo testes porque co2 (odin) pediu

---
+++ Weather.Tests/WheaterApiTest.cs
@@ -0,0 +1,33 @@
+using System;
+using Weather.Bot.Integrations;
+using Xunit;
+
+namespace Weather.Tests
+{
+    public class WheaterApiTest
+    {
+        private readonly IWeatherApi _WeatherApi;
+
+        public WheaterApiTest()
+        {
+            _WeatherApi = new WeatherApi();
+        }
+        [Fact]
+        public void GivenAValidTokenAndUFShouldReturnSpecifiRegion()
+        {
+            var response = _WeatherApi.GetCurrentWeather("2e626cae8c03409289d11817201610", "AM");
+
+            Assert.True(response.Location.Region == "Amazonas");
+        }
+
+        [Fact]
+        public void GivenAValidTokenAndInvalidUFShouldReturnAnException()
+        {
+            var message = "Digita a uf direito co2";
+            var exception = Record.Exception(() => _WeatherApi.GetCurrentWeather("2e626cae8c03409289d11817201610", "XP"));
+            Assert.NotNull(exception);
+            Assert.True(exception.Message == message);
+        }
+
+    }
+}
