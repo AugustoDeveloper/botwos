8e60d4abb8f4f9aa2856d593de4856a6b6eacfdd
Augusto <dev.augusto@gmail.com>
2020-10-15 22:53:29 -0300

    Initial commit

---
+++ src/Weather.Bot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+
+namespace Weather.Bot
+{
+    class Program
+    {
+        static private DiscordSocketClient client;
+
+        async static Task Main(string[] args)
+        {
+            try
+            {
+                client = new DiscordSocketClient();
+
+                client.Log += LogAsync;
+                var token = "";
+                await client.LoginAsync(TokenType.Bot, token);
+                await client.StartAsync();
+
+                Console.ReadKey();
+            }
+            finally
+            {
+                await client.LogoutAsync();
+                await client.StopAsync();
+            }
+            Console.ReadKey();
+        }
+
+        static private Task LogAsync(LogMessage entry)
+        {
+            Console.WriteLine(entry);
+            return Task.CompletedTask;
+        }
+    }
+}
